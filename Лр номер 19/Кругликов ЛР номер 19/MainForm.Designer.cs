cbd4540b5154bb06d65d494aac0d58e10f993f63
eugene <136128227+evgeniikruglikov@users.noreply.github.com>
2024-12-02 12:49:55 +0300

    Add files via upload

---
+++ Лр номер 19/Кругликов ЛР номер 19/MainForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace LINQ_querries
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(363, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(425, 381);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(686, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Освободить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(578, 412);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Забронировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 25);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(177, 355);
+            this.listBox2.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 389);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(136, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "С ребенком до 12 лет";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.HorizontalScrollbar = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(211, 132);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(146, 274);
+            this.checkedListBox1.TabIndex = 6;
+            this.checkedListBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.checkedListBox1_MouseMove);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(211, 67);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(122, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Только свободные";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Удобства:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(211, 41);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Количество мест:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Постоянные покупатели:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Найдено: 0, Свободно: 0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 443);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "MainForm";
+            this.Text = "Задание №19 выполнил: Кругликов Е.А., Номер варианта: 8 Дата выполнения: 24/11/20" +
+    "24";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Лр номер 19/Кругликов ЛР номер 19/MainForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LINQ_querries
+{
+    public partial class MainForm : Form
+    {
+        static List<RoomOption> commonOptions;
+
+        List<Room> rooms;
+        List<RegularCustomer> regularCustomers;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            LoadCSV();
+
+            listBox2.DataSource = regularCustomers;
+            checkedListBox1.DataSource = commonOptions;
+
+            UpdateRooms();
+        }
+
+        public static RoomOption GetCommonOption(string name)
+        {
+            return commonOptions.FirstOrDefault(op => op.Name == name);
+        }
+
+        private void LoadCSV()
+        {
+            commonOptions = File.ReadAllText("RoomOptions.csv", System.Text.Encoding.UTF8).Split('\n').Select(s => { string[] s1 = s.Split(','); return new RoomOption(s1[0], int.Parse(s1[1])); }).ToList();
+            rooms = File.ReadAllText("Rooms.csv", System.Text.Encoding.UTF8).Split('\n').Select(s => { string[] s1 = s.Split(','); return new Room(int.Parse(s1[0]), int.Parse(s1[1]), int.Parse(s1[2]), (s1.Count() > 3) ? s1.Skip(3).Select(opName => GetCommonOption(opName)).ToList() : null); }).ToList();
+            regularCustomers = File.ReadAllText("RegularCustomers.csv", System.Text.Encoding.UTF8).Split('\n').Select(s => { string[] s1 = s.Split(','); return new RegularCustomer(s1[0], int.Parse(s1[1])); }).ToList();
+        }
+
+        private void UpdateRooms()
+        {
+            List<Room> filtered = rooms.Where(r => r.Beds >= numericUpDown1.Value && checkedListBox1.CheckedItems.Cast<RoomOption>().All(op => r.Options.Contains(op)) && (!checkBox2.Checked || !r.Booked)).ToList();
+            listBox1.DataSource = filtered;
+            label4.Text = $"Найдено: {filtered.Count()}, Свободно: {filtered.Count(r => !r.Booked)}";
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateRooms();
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateRooms();
+        }
+
+        private void checkedListBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            UpdateRooms();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Room room = (Room)listBox1.SelectedItem;
+            if (room == null)
+            {
+                MessageBox.Show("Комната не выбрана!");
+                return;
+            }
+
+            if (room.Booked)
+            {
+                room.Booked = false;
+                MessageBox.Show("Комната освобождена!");
+            }
+            else
+                MessageBox.Show("Комната уже свободна!");
+
+            UpdateRooms();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Room room = (Room)listBox1.SelectedItem;
+            if (room == null)
+            {
+                MessageBox.Show("Комната не выбрана!");
+                return;
+            }
+
+            if (room.Booked)
+                MessageBox.Show("Комната уже занята!");
+            else
+            {
+                room.Booked = true;
+                MessageBox.Show($"Комната забронирована! К оплате {Math.Floor(room.Price * (1 - ((((RegularCustomer)listBox2.SelectedItem)?.Discount ?? 0) + (checkBox1.Checked ? 10 : 0)) / 100f))}₽.");
+            }
+
+            UpdateRooms();
+        }
+    }
+}
